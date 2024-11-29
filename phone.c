8d6a1ed7a2d5fdc5788336341b2aa9b2061adf64
Siddhant <shaurya.sid1729@gmail.com>
2024-11-30 01:40:00 +0530

    Add files via upload

---
+++ phone.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+   printf("   #########################################\n");
+printf("      #########################################\n");
+printf("      ##                   O                 ##\n");
+printf("      ##                                     ##\n");
+printf("      ##              SPARKIX                ##\n");
+printf("      ##            STOP TO SHOP             ## \n");
+printf("      ##                                     ##\n");
+printf("      ##  choose the device you want to have ##\n");
+printf("      ##                                     ##\n");
+printf("      ##   1)SMART PHONES                    ##\n");
+printf("      ##   2)LAPTOPS                         ##\n");
+printf("      ##    3)TABS                           ##\n");
+printf("      ##   4)HEADPHONES                      ##\n");
+printf("      ##                                     ##\n");
+printf("      ##                                     ##\n");
+printf("      ##                                     ##\n");
+printf("      ##                                     ##\n");
+printf("      ##                                     ##\n");
+printf("      ##                                     ##\n");
+printf("      ##                                     ##\n");
+printf("      #########################################\n");
+printf("      #########################################\n");
+
+
+
+
+}
